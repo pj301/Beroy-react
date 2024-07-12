aa6d0bf9bdedfd1dc2d0201e547e0c44ea09446c
pj301 <beroypeter143@gmail.com>
2024-07-12 16:30:05 +0800

    first React Activity 07-12-2024

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <p>CC313 REACT PROJECT</p>
+      <p>MEMBERS</p>
+      <div className="box">
+        <h1>PETER JOHN Z. BEROY</h1>
+        <h2>JOHN PAUL B. CANO</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
